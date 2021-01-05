f233b7fb832ea4d514833309162469f0af36f120
Antony <antony.derache@epitech.eu>
2021-01-05 19:36:22 +0100

    [+] fix spelling

--- src/Views/pages/projects.js
+++ src/Views/pages/projects.js
@@ -7,10 +7,10 @@ const Projets = () => {
             <h2 className="fl full-width">Projets</h2>
             <div className="project-content fl full-width">
                 <Card id="skipty" classname="sk" title="Skipty" projet="Projet Stage" type="Site web"/>
-                <Card id="united" classname="uj" title="United Jam" projet="Game Jam" type="jeu vidéo"/>
-                <Card id="global-jam" classname="ggj" title="Epitech Jam" projet="Game Jam inter Epitech" type="jeu vidéo"/>
+                <Card id="united" classname="uj" title="United Jam" projet="Game Jam" type="Jeu vidéo"/>
+                <Card id="global-jam" classname="ggj" title="Epitech Jam" projet="Game Jam inter Epitech" type="Jeu vidéo"/>
                 <Card id="epic" classname="epicture" title="Epicture" projet="Projet 3e année" type="App web type imgur"/>
-                <Card id="indie" classname="bomberman" title="Bomberman" projet="Projet graphique 2e année" type="jeu vidéo (Bomberman)"/>
+                <Card id="indie" classname="bomberman" title="Bomberman" projet="Projet graphique 2e année" type="Jeu vidéo (Bomberman)"/>
                 <Card id="epytodo" classname="epytodo" title="Epytodo" projet="Projet 1er année" type="Site web todolist"/>
             </div>
         </div>

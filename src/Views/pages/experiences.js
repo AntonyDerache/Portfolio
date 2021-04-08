88dbb2e278558fa9429a4f275396ea9f25afbc9b
Antony <antony.derache@epitech.eu>
2021-04-08 15:00:53 +0200

    [+] add sobook to experiences. [~] add propTypes package

--- src/Views/pages/experiences.js
+++ src/Views/pages/experiences.js
@@ -1,51 +1,68 @@
 import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import PropTypes from 'prop-types';
 import 'react-vertical-timeline-component/style.min.css';
+
+const ExperienceElement = (props) => {
+    return (
+         <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: props.backColor, color: 'black' }}
+            contentArrowStyle={{ borderRight: `7px solid ${props.backColor}` }}
+            iconStyle={{ background: props.backColor, color: '#fff' }}
+        >
+            <h3 className="vertical-timeline-element-title">{props.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{props.subtitle}</h4>
+            <p>
+                {props.description}
+            </p>
+        </VerticalTimelineElement>
+    )
+}
+
+ExperienceElement.propTypes = {
+    backColor: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    description: PropTypes.string,
+}
 
 const Experiences = (props) => {
     return (
         <div id="experiences" className="pages full-width full-height padding-sections">
             <h2 className="fl full-width" style={{ paddingBottom: 50 }}>{props.t('experiences.title')}</h2>
             <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(0, 209, 199)', color: '#000' }}
-                    contentArrowStyle={{ borderRight: '7px solid rgb(0, 209, 199)' }}
-                    iconStyle={{ background: 'rgb(0, 209, 199)', color: '#fff' }}
-                >
-                    <h3 className="vertical-timeline-element-title">2020 - 2021</h3>
-                    <h4 className="vertical-timeline-element-subtitle">{props.t('experiences.trackap.job')}</h4>
-                    <p>
-                        {props.t('experiences.trackap.description')}
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(255, 255, 255)', color: 'black' }}
-                    contentArrowStyle={{ borderRight: '7px solid #fff' }}
-                    iconStyle={{ background: '#fff', color: '#fff' }}
-                >
-                    <h3 className="vertical-timeline-element-title">2019</h3>
-                    <h4 className="vertical-timeline-element-subtitle">{props.t('experiences.skipty.job')}</h4>
-                    <p>
-                        {props.t('experiences.skipty.description')}
-                    </p>
-                </VerticalTimelineElement>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    contentStyle={{ background: 'rgb(255, 255, 255)', color: 'black' }}
-                    contentArrowStyle={{ borderRight: '7px solid #fff' }}
-                    iconStyle={{ background: '#fff', color: '#fff' }}
-                >
-                    <h3 className="vertical-timeline-element-title">2018</h3>
-                    <h4 className="vertical-timeline-element-subtitle">{props.t('experiences.delpharm.job')}</h4>
-                    <p>
-                        {props.t('experiences.delpharm.description')}
-                    </p>
-                </VerticalTimelineElement>
+                <ExperienceElement
+                    backColor={'rgb(0, 209, 199)'}
+                    title={"2021"}
+                    substitle={props.t('experiences.sobook.job')}
+                    description={props.t('experiences.sobook.description')}
+                />
+                <ExperienceElement
+                    backColor={'#fff'}
+                    title={"2020 - 2021"}
+                    substitle={props.t('experiences.trackap.job')}
+                    description={props.t('experiences.trackap.description')}
+                />
+                <ExperienceElement
+                    backColor={'#fff'} fontColor={'#fff'}
+                    title={"2019"}
+                    substitle={props.t('experiences.skipty.job')}
+                    description={props.t('experiences.skipty.description')}
+                />
+                 <ExperienceElement
+                    backColor={'rgb(0, 209, 199)'} fontColor={'#fff'}
+                    title={"2018"}
+                    substitle={props.t('experiences.delpharm.job')}
+                    description={props.t('experiences.delpharm.description')}
+                />
             </VerticalTimeline>
         </div>
     );
+}
+
+Experiences.propTypes = {
+    t: PropTypes.any,
 }
 
 export default Experiences;

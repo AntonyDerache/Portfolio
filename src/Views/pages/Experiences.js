91117cf2afb5ca6bc8fb96dba8f03bf333387cf0
Antony <antony.derache@epitech.eu>
2021-12-12 19:53:57 +0100

    [~] remove chrono timeline package and get back to the old timeline component for my experiences

--- src/Views/pages/Experiences.js
+++ src/Views/pages/Experiences.js
@@ -3,76 +3,40 @@ import { VerticalTimeline, } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import ExperienceElement from '../../components/ExperienceElement';
 import PresentationScreen from '../../components/PresentationScreen';
-import { Chrono } from "react-chrono";
 
 const Experiences = (props) => {
-    const blue = "rgb(0, 209, 199)";
-    const white = "#fff";
-
-    const items = [{
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-    }, {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-    }]
-
     return (
         <div id="experiences" className="pages w-100 h-100">
             <PresentationScreen title={props.t('experiences.title')} imgClass={"experiences-background"}/>
-            <Chrono
-                items={items}
-                mode="VERTICAL_ALTERNATING"
-                theme={{
-                    primary: "black",
-                    secondary: "white",
-                    cardBgColor: "white",
-                    cardForeColor: "white",
-                    titleColor: "black"
-                }}
-            />
-            {/* <VerticalTimeline>
+            <VerticalTimeline>
                 <ExperienceElement
-                    backColor={blue}
+                    backColor={"#fff"}
                     skillsName={["NodeJS", "ExpressJS", "VueJS", "MongoDB", "Travis"]}
-                    title={props.t('experiences.sobook.date')}
-                    substitle={props.t('experiences.sobook.job')}
+                    title={props.t('experiences.sobook.job')}
+                    substitle={props.t('experiences.sobook.date')}
                     description={props.t('experiences.sobook.description')}
                 />
                 <ExperienceElement
-                    backColor={white}
+                    backColor={"#fff"}
                     skillsName={["ReactJS", "NodeJS", "ExpressJS", "TypeScript", "AWS S3"]}
-                    substitle={props.t('experiences.trackap.job')}
-                    title={props.t('experiences.trackap.date')}
+                    substitle={props.t('experiences.trackap.date')}
+                    title={props.t('experiences.trackap.job')}
                     description={props.t('experiences.trackap.description')}
                 />
                 <ExperienceElement
-                    backColor={white}
+                    backColor={"#fff"}
                     skillsName={["ReactJS", "NodeJS", "ExpressJS"]}
-                    title={props.t('experiences.skipty.date')}
-                    substitle={props.t('experiences.skipty.job')}
+                    title={props.t('experiences.skipty.job')}
+                    substitle={props.t('experiences.skipty.date')}
                     description={props.t('experiences.skipty.description')}
                 />
                 <ExperienceElement
-                    backColor={blue}
-                    title={props.t('experiences.delpharm.date')}
-                    substitle={props.t('experiences.delpharm.job')}
+                    backColor={"#fff"}
+                    title={props.t('experiences.delpharm.job')}
+                    substitle={props.t('experiences.delpharm.date')}
                     description={props.t('experiences.delpharm.description')}
                 />
-                {/* useless element do not fill */}
-                {/* <ExperienceElement
-                    backColor={blue}
-                    title={"none"}
-                    substitle={"none"}
-                    description={"none"}
-                />
-            </VerticalTimeline> */} */}
+            </VerticalTimeline>
         </div>
     );
 }
--- src/components/ExperienceElement.js
+++ src/components/ExperienceElement.js
@@ -9,13 +9,15 @@ const ExperienceElement = (props) => {
             className="vertical-timeline-element--work"
             contentStyle={{ background: props.backColor, color: 'black' }}
             contentArrowStyle={{ borderRight: `7px solid ${props.backColor}` }}
-            iconStyle={{ background: props.backColor, color: '#fff' }}
+            iconStyle={{ background: "#7fa7ec", color: 'black' }}
         >
-            <div className="d-flex align-items flex-wrap py-3">
             { props.skillsName &&
-                props.skillsName.map((elem, i) => <div key={i} className="skillBubble">{elem}</div>)
+                <div className="d-flex align-items flex-wrap py-3">
+                {
+                    props.skillsName.map((elem, i) => <div key={i} className="skillBubble fw-bold">{elem}</div>)
+                }
+                </div>
             }
-            </div>
             <h3 className="vertical-timeline-element-title">{props.title}</h3>
             <h4 className="vertical-timeline-element-subtitle">{props.substitle}</h4>
             <p>

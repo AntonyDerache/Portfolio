73c267c0d1edce9534aa5d26c260b31efe932dfb
Antony <antony.derache@epitech.eu>
2021-12-18 09:25:13 +0100

    [~] rework about text and add contact icons [-] deleted react chrono package [-] deleted prop-types package [~] switch npm run all package to devDependencies [~] update version of react-scripts and node-sass

--- src/views/pages/About.js
+++ src/views/pages/About.js
@@ -10,7 +10,21 @@ const About = ({updateIndex, t}) => {
         <div id="about" className="pages w-100 h-100">
             <PresentationScreen title={[t('about.title')]} />
             <div className="content purple fl">
-                <p>{t('about.description')}</p>
+                <div className="container">
+                    <p className="pb-5">{t('about.description')}</p>
+                    <div className="d-flex flex-column justify-content-center align-items-center flex-md-row py-4">
+                        <a href="https://github.com/AntonyDerache" rel="noreferrer" target="_blank">
+                            <i className="pe-md-5 bi bi-github"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/antony-derache-9780b5181/" rel="noreferrer" target="_blank">
+                            <i className="pe-md-5 bi bi-linkedin"></i>
+                        </a>
+                        <div>
+                            <i className="pe-4 bi bi-envelope-fill"></i>
+                            <span>antony.derache@epitech.eu</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
